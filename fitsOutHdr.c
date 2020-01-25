d0966b5d32526af460659e8515a04474c2a30451
Sanjana Prabhu Desai <spdesai@bremusa.(none)>
2020-01-24 20:07:09 -0800

    as per aosoft

--- fitsOutHdr.c
+++ fitsOutHdr.c
@@ -1,7 +1,7 @@
 #include	<stdio.h>
 #include	<unistd.h>
-#define OK 0
-#define ERROR -1  
+#include <utilLib.h>
+
 /*
  *	output fits header
  *	if ok, return 0
--- makefits.c
+++ makefits.c
@@ -1,7 +1,7 @@
 #include	<stdio.h>
 #include	<stdlib.h>
 #include	<string.h>
-#include	"utilLib.h"
+#include	<utilLib.h>
 #define STDOUT 1
 #define TRUE 1
 #define FALSE 0
