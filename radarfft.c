51c1027164f17425611b19d85b9008e0738f6f3c
Sanjana Prabhu Desai <spdesai@bremusa.(none)>
2020-01-28 16:45:09 -0800

    radarfft.c check

--- radarfft.c
+++ radarfft.c
@@ -1,8 +1,12 @@
 #include        <stdio.h>
+#include		<stdlib.h>
 #include        <fcntl.h>
 #include        <malloc.h>
 #include		<unistd.h>
-#include		<math.h>
+#include		<fftw3.h>
+#include	    <math.h>
+#include		<philLib.h>
+ 
 #define STDOUT 1
 #define TRUE 1
 #define FALSE 0
@@ -20,23 +24,18 @@ typedef struct {
 	float	*fftBlk;		/* holds input data, fft output*/
 	float	*avgBuf;		/* holds averaged power*/
 	int		packed;		    /* 0-no, 1,2,4 bytes integer data*/
+	int     cmplxFft;       /* if not 0 then output complex fft data. no power, no averaging, only 1fft*/
 	int		bytesInpReq;		/* for input*/
 	int		bytesInpBlk;	/* after unpacking*/
 	int		bytesOutReq;	/* for output after power*/
-        float   xoff; /* apply frequency offset xoff [Hz, assuming 1 s per cmplx sample] during FFT */
+    double  tmSmp;          /* time (secs) for 1 sample. only needed if -x usec*/
+    double  xoff;           /* apply frequency offset xoff [Hz, assuming 1 s per cmplx sample] during FFT */
 	} RDRFFTI;
 
 void    processargs(int argc,char **argv,RDRFFTI *pinfo);
 void    swap_freq(float *data, int len);
-void    apply_linear_phase(float *data, double freq, double time, double timeint, int nsamples);
-extern int  read_pipe();
-#if FALSE
-extern void four1_();
-#endif
-
-/* revision control variable */
-static char const rcsid[] = 
-"$Id$";
+void    apply_linear_phase(float *data, double freq, double time, 
+			double timeint, int nsamples);
 
 int main(int argc,char **argv)
 {
@@ -46,26 +45,30 @@ int main(int argc,char **argv)
  *		if we have only a partially full fft at the end, throw it 
  *      away unless it is the only block, then we will zero fill it.
  *
- *       rdrfft  -l lenfft -r numrangebins -p 1,2,4
+ *       radarfft  -c -l lenfft -r numrangebins -p packing -x freqCorHz
  *
- *      -l  len fft
- *      -r  number of range bins
- *      -p  packed 1,2,or 4 byte integer data
+ *      -c  output complex fft data. only  one fft*numberRangebins will be output
+ *          no power or incoherent averaging will be done. Mainly used to later compute stokes
+ *          parameters. (def; output avged power spectra)
+ *      -l  len fft (def=1024)
+ *      -r  number of range bins in data (def=1000)
+ *      -p  packedIntegerData:1,2,or 4 byte integer.(def=assume 4byte float inputs)
+ *      -t  time sample (secs) def:1e-6. time for each sample (only required/used if -x used)
+ *      -x  freqOffset (hz) to apply to input data. (need to verify that sign of rotation is correct).
  *  history:
+ * after 28oct10 see svn log radarfft.c
  * 25may99 - stole from fftfilter
  * 20jun99 - added -p 1,2,4 bytes.. for 1,2,4 byte integer data
  *		     use for decoder input data
  *  5aug99 - converted to fftwAo
  *  1nov99 - fftwAo now has not inplace option.
- *  jlm added swap_freq()
- *  jlm added capability to apply frequency offset w apply_linear_phase()
 */
         int     xformdir;                       /* +/-1*/
 		int		blk;
         int     bytes_inp;
 		int	    rowLenFlt;					/* row lenght flts before transf*/
-		int	    rowLenPnt;					/* in complex points*/
-		int	    numRowsInp; 				/* in floats*/
+//		int	    rowLenPnt;					/* in complex points*/
+//		int	    numRowsInp; 				/* in floats*/
 		int		numFltPwr;			        /* number of floats power array*/
 		float	*pc,*pinp,*pout,*pa;
 		int		ih,iv,i;					/* horizontal, veritical in*/
@@ -76,16 +79,20 @@ int main(int argc,char **argv)
 		int		*pi;
 		short	*psh;
 		char    *pch;
+		fftwf_plan fftwPlan;
 
 /*
  *      get the parameters
 */
         processargs(argc,argv,&info);
-        xformdir = 1; 		/* this is a forward transform*/
+        xformdir = 1; 		/* this is the sign of exponent. called a reverse xform*/
+        fftwPlan=fftwf_plan_dft_1d(info.lenfft,
+                (fftwf_complex*)info.fftBlk,
+                (fftwf_complex*)info.fftBlk,xformdir,FFTW_MEASURE);
 
 		rowLenFlt =info.numbins*2;
-		rowLenPnt =info.numbins; 	/* number complex points*/
-		numRowsInp=info.lenfft;
+//		rowLenPnt =info.numbins; 	/* number complex points*/
+//		numRowsInp=info.lenfft;
 		numFltPwr =info.numbins*info.lenfft;
 		firstTime = 1;
 
@@ -164,17 +171,19 @@ info.lenfft,info.numbins,info.bytesInpBlk,info.bytesOutReq);
 			* now loop doing the transforms
 			*/
 			for (i=0,pc=info.fftBlk;i<info.numbins;i++,pc+=info.lenfft*2){
-
-		if (info.xoff != 0) apply_linear_phase(pc, info.xoff, 0.0, 1.0, info.lenfft);		  
-             	fftwAo(pc,NULL,info.lenfft,xformdir,1);
-/* 
-             	four1_(pc,&info.lenfft,&xformdir);
-*/
-			    /* swap frequencies */
-			    swap_freq(pc,info.lenfft);
+             	if (info.xoff != 0.)
+				 apply_linear_phase(pc, info.xoff, 0.0, info.tmSmp, info.lenfft);
+             	fftwf_execute_dft(fftwPlan,(fftwf_complex*)pc,(fftwf_complex*)pc);
+				/* swap frequencies */
+                swap_freq(pc,info.lenfft);
+			}
+			// if complex fft ,just 1 blk,  no power or averaging.. just leave data in fft buf
+			if (info.cmplxFft != 0) {
+				blk=1;				// so output message at end is correct.
+				break;
 			}
 			/*
-			 * add the values into average array
+		 	* add the values into average array
 			*/
 			if (firstTime) {
 		    	for (pa=info.avgBuf,pc=info.fftBlk;
@@ -193,7 +202,7 @@ info.lenfft,info.numbins,info.bytesInpBlk,info.bytesOutReq);
 		/*
 		 * now scale to number of blocks
 		*/
-		if (blk > 1) {
+		if ((info.cmplxFft == 0 ) && (blk > 1)) {
 			scale=1./blk;
 			for (pa=info.avgBuf;pa< (info.avgBuf+numFltPwr);pa++){
 				*pa= (*pa)*scale;
@@ -202,36 +211,45 @@ info.lenfft,info.numbins,info.bytesInpBlk,info.bytesOutReq);
 	/*
  	*      now write out the xform
 	*/
-done: bytes_out = write(STDOUT,(char *)info.avgBuf,info.bytesOutReq);
+done: 
+	  if (info.cmplxFft) {
+	  	bytes_out = write(STDOUT,(char *)info.fftBlk,info.bytesOutReq);
+	  } else {
+	  	bytes_out = write(STDOUT,(char *)info.avgBuf,info.bytesOutReq);
+	  }
       if (info.bytesOutReq != bytes_out){
                fprintf(stderr,"radarfft: output error. bytes out %d\n",
 			   bytes_out);
                exit(-1);
       }
-      fprintf(stderr,"%d blocks of %d lenfft x %d bins xformed and added\n",
+	  if (info.cmplxFft) {
+      	fprintf(stderr,"%d blocks of %d lenfft x %d bins xformed and output as complex values\n",
 				blk,info.lenfft,info.numbins);
-             exit(0);
+	  } else {
+      	fprintf(stderr,"%d blocks of %d lenfft x %d bins xformed and added\n",
+				blk,info.lenfft,info.numbins);
+	  }
+      exit(0);
         /*NOTREACHED*/
 }       
-
 /******************************************************************************/
-/*	apply_linear_phase						      */
+/*  apply_linear_phase                            */
 /******************************************************************************/
 void apply_linear_phase(float *data, double freq, double time, double timeint, int nsamples)
-/* data		 data array */
-/* freq		 linear phase correction, Hz */
-/* time		 start time of phase correction */
-/* timeint	 sample period */
-/* nsamples	 number of complex samples in data array */
+/* data      data array */
+/* freq      linear phase correction, Hz */
+/* time      start time of phase correction */
+/* timeint   sample period */
+/* nsamples  number of complex samples in data array */
 {
   /* apply a linear phase correction of freq Hz to the data using the
      sample time spacing timeint
   */
   int i,j;
-  float  phase_r, phase_i;	/* real and imag phase components */
-  float  data_r, data_i;	/* real and imag data components */
-  double phase;			/* phase correction in radians */
-  double freq_rad;		/* frequency in rad/sec */
+  float  phase_r, phase_i;  /* real and imag phase components */
+  float  data_r, data_i;    /* real and imag data components */
+  double phase;         /* phase correction in radians */
+  double freq_rad;      /* frequency in rad/sec */
   double t;
 
   t        = time;
@@ -253,9 +271,8 @@ void apply_linear_phase(float *data, double freq, double time, double timeint, i
   }
   return;
 }
-
 /******************************************************************************/
-/*	swap_freq							      */
+/*  swap_freq                                 */
 /******************************************************************************/
 void swap_freq(float *data, int len)
 /* spectrum array from fft */
@@ -267,7 +284,7 @@ void swap_freq(float *data, int len)
   int i,j;
   float temp;
 
-  for (i=0, j=len; i<len; i++, j++) 
+  for (i=0, j=len; i<len; i++, j++)
     {
       temp    = data[i];
       data[i] = data[j];
@@ -276,7 +293,6 @@ void swap_freq(float *data, int len)
 
   return;
 }
-
 /******************************************************************************/
 /*      processargs                                                           */
 /******************************************************************************/
@@ -304,30 +320,45 @@ void    processargs
         extern int opterr;              /* if 0, getopt won't output err mesg*/
 
         int c;                          /* Option letter returned by getopt*/
-        char  *myoptions = "l:r:p:x:";   /* options to search for. :--> needs
+        char  *myoptions = "cl:r:p:x:t:";   /* options to search for. :--> needs
                                             an argument*/
         char *USAGE =
-   "Usage: radarfft  -l lenffts -r rangeBins -p packed(1-4) [-x freq offset (Hz based on 1 Hz)] ";
+"Usage: radarfft  -c -l lenffts -r rangeBins -p packed(1-4) -x freq_offset\n\
+                 -c            output complex fft. Only 1 fft, no power or incoherent avgs.\n\
+                 -l lenffts    length of fft to use (def=1024).\n\
+                 -r rangeBins  number of rangeBins in data (def=1000)\n\
+                 -p intPacking type of packed integer data:1,2,4 (def:4byteFloat)\n\
+                 -t timeSmp    time sample in secs (def=1e-6). Only needed for -x option\n\
+                 -x freqOffHz  frequency correction (hz) to make to the data\n";
 
         opterr = 0;                             /* turn off there message*/
 		pI->lenfft =1024;
 		pI->numbins=1000;
 		pI->numBlksInp=0;
 		pI->packed    =0;
-		pI->xoff      =0;
+		pI->xoff      =0.;
+		pI->cmplxFft  =0.;
+		pI->tmSmp     =-1.;
 /* 
         loop over all the options in list
 */
         while ((c = getopt(argc,argv,myoptions)) != -1){ 
           switch (c) {
+          case 'c':
+				   pI->cmplxFft  =1.;
+				   break;
           case 'x':
-                   sscanf(optarg,"%f",&pI->xoff);
+                   sscanf(optarg,"%lf",&pI->xoff);
                    break;
           case 'l':
                    sscanf(optarg,"%d",&pI->lenfft);
+
                    break;
           case 'r':
                    sscanf(optarg,"%d",&pI->numbins);
+                   break;
+          case 't':
+                   sscanf(optarg,"%lf",&pI->tmSmp);
                    break;
           case 'p':
                    sscanf(optarg,"%d",&pI->packed);
@@ -339,11 +370,18 @@ void    processargs
           }
         }
 /*
+ * xoff requires user to specify time sample
+*/
+	if ((pI->xoff != 0.) && (pI->tmSmp < 0.)) {
+        fprintf(stderr,"Err-->  -x xoff requires you also specify -t tmSmpSec\n");
+	    goto errout;
+	}
+/*
  * compute size of bufs then allocate them
  * input  lenrange*lenfft*size(float)*2(since comples)
 */
 		pI->bytesInpBlk=sizeof(float)*2*pI->lenfft*pI->numbins;
-		pI->bytesOutReq=pI->bytesInpBlk/2; /* since we avg power*/
+		pI->bytesOutReq=(pI->cmplxFft)? pI->bytesInpBlk: pI->bytesInpBlk/2; /* not cmplx /2 since avg i,q)*/
 		if ((pI->inpBlk=(float*)malloc(pI->bytesInpBlk)) == NULL) {
 			perror("radarfft.. mallocing inputblk buffer");
 			goto errout;
@@ -352,11 +390,12 @@ void    processargs
 			perror("radarfft.. mallocing fft buffer");
 			goto errout;
 		}
-		if ((pI->avgBuf=(float*)malloc(pI->bytesOutReq)) == NULL) {
-			perror("radarfft.. mallocing avgerage buffer");
-			goto errout;
+		if (pI->cmplxFft == 0) {
+			if ((pI->avgBuf=(float*)malloc(pI->bytesOutReq)) == NULL) {
+				perror("radarfft.. mallocing avgerage buffer");
+				goto errout;
+			}
 		}
-
 		pI->bytesInpReq=pI->bytesInpBlk;
 	 	if (pI->packed == 0){
 			pI->pinpBuf=(char*)pI->inpBlk;	/* use same buf*/
@@ -378,7 +417,6 @@ void    processargs
 /*
         here if illegal option or argument
 */
-errout: fprintf(stderr,"%s\n",rcsid);
-        fprintf(stderr,"%s\n",USAGE);
+errout: fprintf(stderr,"%s\n",USAGE);
         exit(1);
 }
