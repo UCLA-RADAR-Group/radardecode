0410218505f0e46ae59f99b1a3efadca61beb112
Jean-Luc Margot <jlm@epss.ucla.edu>
2020-06-23 15:43:01 -0700

    Test fractional precision not absolute precision in validation script.

--- validation/validate.sh
+++ validation/validate.sh
@@ -8,33 +8,25 @@ u4_param_1=0
 cd cw
 drv_cw.sc drvcw.result 1 1
 
-od -f test_64.p1 > test.tmp1
-od -f result_64.p1 > result.tmp1
+od -f test_64.p1 | head > test.tmp1
+od -f result_64.p1 | head > result.tmp1
 
-head test.tmp1 | awk '{print $2}' > test_hd2p1.cmp
-tail test.tmp1 | awk '{print $4}' > test_tl4p1.cmp
-head result.tmp1 | awk '{print $2}' > result_hd2p1.cmp
-tail result.tmp1 | awk '{print $4}' > result_tl4p1.cmp
+cat test.tmp1 | awk '{print $2}' > test_hd2p1.cmp
+cat result.tmp1 | awk '{print $2}' > result_hd2p1.cmp
 
 paste test_hd2p1.cmp result_hd2p1.cmp > hd2p1.cmp
-paste test_tl4p1.cmp result_tl4p1.cmp > tl4p1.cmp
 
-cat hd2p1.cmp | awk '{if(($1-$2)>10000 || ($1-$2)<-10000) print ($1-$2)}' > err
-cat tl4p1.cmp | awk '{if(($1-$2)>10000 || ($1-$2)<-10000) print ($1-$2)}' >> err
+cat hd2p1.cmp | awk '{if(($1-$2)/$1>0.0001 || ($1-$2)/$1<-0.0001) print ($1-$2)}' > err
 
-od -f test_64.p2 > test.tmp2
-od -f result_64.p2 > result.tmp2
+od -f test_64.p2 | head > test.tmp2
+od -f result_64.p2 | head > result.tmp2
 
 head test.tmp2 | awk '{print $2}' > test_hd2p2.cmp
-tail test.tmp2 | awk '{print $4}' > test_tl4p2.cmp
 head result.tmp2 | awk '{print $2}' > result_hd2p2.cmp
-tail result.tmp2 | awk '{print $4}' > result_tl4p2.cmp
 
 paste test_hd2p2.cmp result_hd2p2.cmp > hd2p2.cmp
-paste test_tl4p2.cmp result_tl4p2.cmp > tl4p2.cmp
 
-cat hd2p2.cmp | awk '{if(($1-$2)>10000 || ($1-$2)<-10000) print ($1-$2)}' >> err
-cat tl4p2.cmp | awk '{if(($1-$2)>10000 || ($1-$2)<-10000) print ($1-$2)}' >> err
+cat hd2p2.cmp | awk '{if(($1-$2)/$1>0.0001 || ($1-$2)/$1<-0.0001) print ($1-$2)}' >> err
 
 
 if [ $(du -k err | cut -f1) -eq "0" ];then # if err is 0 then cw works
@@ -52,20 +44,20 @@ cd ..
 cd u4
 export DRVSB=drv.1k
 drv_all.sc 1 1
-drv_mapmsc.sc 1 1 1 1
-mv Fits/f1x1.fit Fits/result.fits
+#drv_mapmsc.sc 1 1 1 1
+#mv Fits/f1x1.fit Fits/result.fits
 
 # unscaled images
-od -f test.1k.mapf1 | head > test.tmp1
-od -f result.mapf1 | head > result.tmp1
+# od -f test.1k.mapf1 | head > test.tmp1
+# od -f result.mapf1 | head > result.tmp1
 
-cat test.tmp1 | awk '{print $2}' > test_hd2.cmp
-cat result.tmp1 | awk '{print $2}' > result_hd2.cmp
+# cat test.tmp1 | awk '{print $2}' > test_hd2.cmp
+# cat result.tmp1 | awk '{print $2}' > result_hd2.cmp
 
-paste result_hd2.cmp test_hd2.cmp > unscaled.cmp
+# paste test_hd2.cmp result_hd2.cmp > unscaled.cmp
 
-cat unscaled.cmp | awk '{if(($1-$2)>0.0001 || ($1-$2)<-0.0001) print ($1-$2)}' > err
-if [ $(du -k err | cut -f1) -eq "0" ];then u4_param_1=1; else u4_param_1=0; fi
+# cat unscaled.cmp | awk '{if(($1-$2)/$2>0.0001 || ($1-$2)/$2<-0.0001) print ($1-$2)}' > err
+# if [ $(du -k err | cut -f1) -eq "0" ];then u4_param_1=1; else u4_param_1=0; fi
 
 
 # scaled images
@@ -75,9 +67,9 @@ od -f result.mapsf1 | head > result.tmp1
 cat test.tmp1 | awk '{print $2}' > test_hd2.cmp
 cat result.tmp1 | awk '{print $2}' > result_hd2.cmp
 
-paste result_hd2.cmp test_hd2.cmp > scaled.cmp
+paste test_hd2.cmp result_hd2.cmp > scaled.cmp
 
-cat scaled.cmp | awk '{if(($1-$2)>0.0001 || ($1-$2)<-0.0001) print ($1-$2)}' > err
+cat scaled.cmp | awk '{if(($1-$2)/$1>0.0001 || ($1-$2)/$1<-0.0001) print ($1-$2)}' > err
 if [ $(du -k err | cut -f1) -eq "0" ];then u4_param_1=1; else u4_param_1=0; fi
 
 
